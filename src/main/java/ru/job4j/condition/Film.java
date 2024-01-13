031710a74e2b8ad0beeb6559c2e09cd68a02ea9b
JenBrainnet <benebrak01@gmail.com>
2024-01-13 20:37:27 +0200

    4.2. Тернарное сравнение [#189 #473223] set up jdk-17

--- src/main/java/ru/job4j/condition/Film.java
+++ src/main/java/ru/job4j/condition/Film.java
@@ -1,7 +1,6 @@
 package ru.job4j.condition;
 
 public class Film {
-
     public static String permission(int age) {
         boolean can = age >= 18;
         String label = can ? "Please, enjoy it." : "You can't watch it.";
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 class MaxTest {
-
     @Test
     void whenMax1To2Then2() {
         int left = 1;
