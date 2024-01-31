c877b102c2dc5427a27b3a323286768e4b1ac794
JenBrainnet <benebrak01@gmail.com>
2024-01-31 16:33:42 +0200

    5.3. Построить шахматную доску в псевдографике. [#13559]

---
+++ src/main/java/ru/job4j/loop/NestedLoop.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+public class NestedLoop {
+
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.println("Значение индекса внешнего цикла: " + i
+                        + " и значение индекса вложенного цикла: " + j
+                );
+                count++;
+            }
+        }
+        System.out.println("Количество итераций: " + count);
+    }
+
+}
