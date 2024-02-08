9903c4ece97bd7c7a66358c0c1f09bae3ab5f45b
JenBrainnet <benebrak01@gmail.com>
2024-02-08 20:27:20 +0200

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -9,6 +9,25 @@ public class ArrayDefinition {
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+
+        int[] numbers = new int[5];
+        numbers[0] = 2006;
+        numbers[1] = 2010;
+        numbers[2] = 2014;
+        numbers[3] = 2018;
+        numbers[4] = 2022;
+        int years = numbers[3];
+        System.out.println(years);
+
+        String[] names = new String[4];
+        names[0] = "Petr";
+        names[1] = "Stas";
+        names[2] = "Alexey";
+        names[3] = "Andrei";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 
 }
